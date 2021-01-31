33f9c743f4e8c8e9455fce2033012075e05037a3
@Julia1980 <vol-nina@yandex.ru>
2021-01-31 13:21:38 +0300

    Изменена функция vvod_info_pos()

--- Positions/views.py
+++ Positions/views.py
@@ -2,7 +2,10 @@ from django.forms import model_to_dict
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+
+# from .func_for_views import vvod_info_pos
 from .models import Positions
+import sqlite3
 
 
 
@@ -10,3 +13,6 @@ def index(request):
     positions = Positions._meta.get_fields()
     print(positions)
     return HttpResponse(f'{positions}')
+
+
+
