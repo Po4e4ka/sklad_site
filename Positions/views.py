d6f73591deb621477f34327d011ca1ee8f963510
Po4e4ka <toolset@live.ru>
2021-02-01 17:40:12 +0300

    Добавлено отображение данных таблице в пути host/list

--- Positions/views.py
+++ Positions/views.py
@@ -16,4 +16,28 @@ class NewView(View):
         return render(request, 'Positions/new_position.html')
 
     def post(self, request):
+        # реобразование в словарь всех данных из пост
+        main_dict = dict(request.POST.items())
+        # Создание нового автора, если такого нет в таблице Авторы или сслыка на существующую запись
+        # author = Author.objects.get_or_create(name=main_dict['author'])
+        # if isinstance(author, tuple):
+        #     author = author[0]
+        # Создание новой книги с информацией, полученной из формы
+        # book = Book.objects.create(name=main_dict['name'],
+        #                            publishDate=main_dict['publishDate'],
+        #                            genre=main_dict['genre'],
+        #                            author_id=author)
+        # book.save()
+        html = "<html><body>Успешно добавлено<br>"
+        for key, item in main_dict.items():
+            html += f"{key}: {item}<br>"
+        html += '<input type="button" onclick="history.back();" value="Назад"/></body></html>'
+        return HttpResponse(html)
+
+class PositionLisView(View):
+    def get(self, request):
+        positions = Positions.objects.all()
+        positions = [model_to_dict(pos) for pos in positions]
+        return render(request, "Positions/positions_view_table.html", context={"positions":positions})
+    def post(self, request):
         pass
