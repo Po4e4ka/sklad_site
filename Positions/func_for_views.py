14b290f0424ac0fc199e26414b79bc15d5efb370
@Julia1980 <vol-nina@yandex.ru>
2021-02-03 22:33:25 +0300

    Скорректирована функция ввода в таблицу Positions

--- Positions/func_for_views.py
+++ Positions/func_for_views.py
@@ -1,16 +1,43 @@
+from django.db.models import Max, Avg
 from django.http import HttpResponse
 from Positions.models import Positions
+import pandas as pd
+
+
+def excel_to_dict(file="Positions/Копия 1.xls"):
+    # Load spreadsheet
+    xl = pd.ExcelFile(file)
+    # Load a sheet into a DataFrame by name: df1
+    df1 = xl.parse('номенклатура')
+    result = []
+    for index, row in df1.iterrows():
+        if type(row['номенклатура']) == float:
+            break
+        result.append({"name":row['номенклатура'],"quantity":row["количество"], "ediz":row["ед.измер."]})
+    return result
 
 
 def vvod_info_pos(data: dict):
+    print(data)
     if type(data) != dict:
         return HttpResponse("Bad Request: wrong type of data")
-    if Positions.objects.filter(id=data['id']).count() < 1:
+    # if 'id' in data:
+    #     if Positions.objects.filter(id=data['id']).count() > 0:
+    #         return HttpResponse("Bad Request: positions.id is already in base")
+    # else:
+    #     print()
+    #     data['id'] = Positions.objects.all().aggregate(Max('id'))
+    #     print(data)
+    #     return HttpResponse("OK")
+    try:
         position = Positions(**data)
-        position.save()
-        return HttpResponse(f"Позиция введена: {position.id}, {position.name}")
-    else:
-        return HttpResponse("Bad Request: positions.id is already in base")
 
-def test_vvod():
-    return HttpResponse("lol_test")
+        position.save()
+    except:
+        return -1
+    # return HttpResponse(f"Позиция введена: {position.id}, {position.name}")
+
+
+
+
+
--- Positions/views.py
+++ Positions/views.py
@@ -4,18 +4,14 @@ from django.shortcuts import render
 from django.views import View
 
 # from .func_for_views import vvod_info_pos
-from .func_for_views import vvod_info_pos, test_vvod
+from .func_for_views import vvod_info_pos, excel_to_dict
 from .models import Positions
 import sqlite3
 
 
-data_test = {'id' : 1, 'code' : 1, 'name' : "test", 'quantity' : 10.0}
-
-
 def index(request):
 # positions = Positions._meta.get_fields()
-# print(positions)
-#    return test_vvod() # Это просто проверка, что такой вызов работает из твоего файла
-    return HttpResponse(vvod_info_pos(data_test)) # Здесь идет вызов твоей функции
-# # return HttpResponse(f'{positions}')
+    for i in excel_to_dict():
+        vvod_info_pos(i)
+    return HttpResponse('OK')
 
