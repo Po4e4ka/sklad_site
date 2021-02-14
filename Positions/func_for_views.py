24bfb15e13914e0eb0e5c5598b65a5e0e9d4b86c
@Julia1980 <vol-nina@yandex.ru>
2021-02-14 21:56:43 +0300

    Функция vvod_info_ch_qant работает с 4 видами операций (кроме нулевых изменений)

--- Positions/func_for_views.py
+++ Positions/func_for_views.py
@@ -1,9 +1,11 @@
 # from django.db.models import Max, Avg
+from django.forms import model_to_dict
 from django.http import HttpResponse
 from .models import Positions, Groups, Xyz, Levels, Persons, Objects, \
     Change_types, Change_qantity
 import pandas as pd
-import datetime
+from datetime import datetime
+
 
 def excel_to_dict(file="Positions/Копия 1.xls"):
     # Load spreadsheet
@@ -14,12 +16,14 @@ def excel_to_dict(file="Positions/Копия 1.xls"):
     for index, row in df1.iterrows():
         if type(row['номенклатура']) == float:
             break
-        result.append({"name":row['номенклатура'],"quantity":row["количество"], "ediz":row["ед.измер."]})
+        result.append({"name": row['номенклатура'], "quantity": row["количество"], "ediz": row["ед.измер."]})
     return result
+
 
 def data_test(data):
     if type(data) != dict:
         return HttpResponse("Bad Request: wrong type of data")
+
 
 def vvod_info_pos(data: dict):
     data_test(data)
@@ -32,15 +36,18 @@ def vvod_info_pos(data: dict):
                 position = Positions(**data)
                 position.save()
                 print(f"Сохранено в базе: {position.id}, {position.name}, {position.quantity}")
+                vvod_info_ch_qant(data, type=5)
             except:
                 return -1
     else:
         return HttpResponse("Bad Request: positions.name is absent")
 
-nomenklatura_test =  [{"id": 1, "name": "материалы"},
-                      {"id": 2, "name": "комплектующие и детали"},
-                      {"id": 3, "name": "запасные части"},
-                      {"id": 4, "name": "разное"}]
+
+nomenklatura_test = [{"id": 1, "name": "материалы"},
+                     {"id": 2, "name": "комплектующие и детали"},
+                     {"id": 3, "name": "запасные части"},
+                     {"id": 4, "name": "разное"}]
+
 
 def vvod_info_group(data: dict):
     data_test(data)
@@ -53,14 +60,17 @@ def vvod_info_group(data: dict):
                 group = Groups(**data)
                 group.save()
                 print(f"Сохранено в базе: {group.id}, {group.name}")
+                vvod_info_ch_qant(data, type=5)
             except:
                 return -1
     else:
         return HttpResponse("Bad Request: group.name is absent")
 
-xyz_test =  [{"id": 1, "X": "a", "y": 1, "z": 1},
-             {"id": 2, "X": "a", "y": 2, "z": 1},
-             {"id": 3, "X": "a", "y": 3, "z": 1}]
+
+xyz_test = [{"id": 1, "X": "a", "y": 1, "z": 1},
+            {"id": 2, "X": "a", "y": 2, "z": 1},
+            {"id": 3, "X": "a", "y": 3, "z": 1}]
+
 
 def vvod_info_xyz(data: dict):
     """доделать другую проверку - чтобы выбиралось из закрытого перечня координат"""
@@ -73,18 +83,20 @@ def vvod_info_xyz(data: dict):
             xyz = Xyz(**data)
             xyz.save()
             print(f"Сохранено в базе: {xyz.id}, {xyz.X}, {xyz.z}")
+            vvod_info_ch_qant(data, type=5)
         except:
             return -1
         else:
             return HttpResponse("Bad Request: group.id is not defined")
 
 
-level_test =  [{"id": 1, "name": "top"},
-               {"id": 2, "name": "buh"},
-               {"id": 3, "name": "sklad"},
-               {"id": 4, "name": "another"},
-               {"id": 5, "name": "admin"}
-               ]
+level_test = [{"id": 1, "name": "top"},
+              {"id": 2, "name": "buh"},
+              {"id": 3, "name": "sklad"},
+              {"id": 4, "name": "another"},
+              {"id": 5, "name": "admin"}
+              ]
+
 
 def vvod_info_level(data: dict):
     data_test(data)
@@ -98,16 +110,18 @@ def vvod_info_level(data: dict):
                 level = Levels(**data)
                 level.save()
                 print(f"Сохранено в базе: {level.id}, {level.name}")
+                vvod_info_ch_qant(data, type=5)
             except:
                 return -1
     else:
         return HttpResponse("Bad Request: level.name is not defined")
 
 
-person_test =  [{"id": 1, "name": "Иванов Иван"},
-                {"id": 2, "name": "Петров Петр"},
-                {"id": 3, "name": "Сидоров Сидор"},
-                {"id": 4, "name": "Инкогнито"}]
+person_test = [{"id": 1, "name": "Иванов Иван"},
+               {"id": 2, "name": "Петров Петр"},
+               {"id": 3, "name": "Сидоров Сидор"},
+               {"id": 4, "name": "Инкогнито"}]
+
 
 def vvod_info_person(data: dict):
     data_test(data)
@@ -120,18 +134,19 @@ def vvod_info_person(data: dict):
                 person = Persons(**data)
                 person.save()
                 print(f"Сохранено в базе: {person.id}, {person.name}")
+                vvod_info_ch_qant(data, type=5)
             except:
                 return -1
     else:
         return HttpResponse("Bad Request: person.name is absent")
 
 
-
 obj_test = [
-             {"id": 1, "name": "object1"},
-             {"id": 2, "name": "object2"},
-             {"id": 3, "name": "object3"},
-            ]
+    {"id": 1, "name": "object1"},
+    {"id": 2, "name": "object2"},
+    {"id": 3, "name": "object3"},
+]
+
 
 def vvod_info_obj(data: dict):
     data_test(data)
@@ -144,17 +159,20 @@ def vvod_info_obj(data: dict):
                 obj = Objects(**data)
                 obj.save()
                 print(f"Сохранено в базе: {obj.id}, {obj.name}")
+                vvod_info_ch_qant(data, type=5)
             except:
                 return -1
     else:
         return HttpResponse("Bad Request: objects.name is not defined")
 
+
 change_test = [
-             {"id": 1, "name": "prihod", "znak": True},
-             {"id": 2, "name": "rashod", "znak": False},
-             {"id": 3, "name": "izlishki", "znak": True},
-             {"id": 4, "name": "nedostacha", "znak": False},
-            ]
+    {"id": 1, "name": "prihod", "znak": False},
+    {"id": 2, "name": "rashod", "znak": True},
+    {"id": 3, "name": "izlishki", "znak": False},
+    {"id": 4, "name": "nedostacha", "znak": True},
+]
+
 
 def vvod_info_ch_type(data: dict):
     """закрытый перечень операций, права изменения только у админа"""
@@ -168,28 +186,43 @@ def vvod_info_ch_type(data: dict):
                 ch_type = Change_types(**data)
                 ch_type.save()
                 print(f"Сохранено в базе: {ch_type.id}, {ch_type.name}")
+                vvod_info_ch_qant(data, type=5)
             except:
                 return -1
     else:
         return HttpResponse("Bad Request: change_types.name is not defined")
 
 
-qant_test = {"position_id": 1, "quantity": 50.0, "change_type_id" : 1}
+qant_test = {"position_id": Positions.objects.filter(id=3).get().id, "quantity": 300.0}
+type_test = 4
 
-def vvod_info_ch_qant(data: dict):
+
+def vvod_info_ch_qant(data: dict = qant_test, type=type_test):
     data_test(data)
-    if "position_id" in data:
-        if Positions.objects.filter(id=data["position_id"]).count() > 0:
-            try:
-                ch_qant = Change_qantity(**data)
-                ch_qant.time_oper = datetime.datetime
-                print(ch_qant)
-                ch_qant.save()
-                print(f"Сохранено в базе: {ch_qant.id}, {ch_qant.id.name}, {ch_qant.id.quantity}, {ch_qant.id.quantity}")
-            except:
-                return -1
-        else:
+    # print(data)
+    if type < 5:
+        if Positions.objects.filter(id=data["position_id"]).count() < 1:
             print("Позиция не найдена", data["position_id"])
+            return HttpResponse("Bad Request: Positions.id is not defined")
+        ch_qant = Change_qantity()
+        ch_qant.time_oper = datetime.now()
+        ch_qant.change_type_id = Change_types.objects.filter(id=type).get()
+        ch_qant.position_id = Positions.objects.filter(id=data["position_id"]).get()
+        ch_qant.quantity = data["quantity"]
+
+        print(model_to_dict(ch_qant))
+        ch_qant.save()
+
+        pos = Positions.objects.filter(id=data["position_id"]).get()
+        # print(Change_types.objects.filter(id=type).get().znak)
+        pos.quantity += (-1) ** Change_types.objects.filter(id=type).get().znak * data["quantity"]
+        pos.save()
+        print(pos.quantity)
+        print(f"Сохранено в базе: {pos.id},{pos.name}, {pos.quantity}")
+
     else:
-         print(f"Не определен ID изменяемой позиции {data['name']}")
-         return HttpResponse("Bad Request: positions.id is absent")
+        print("записана операция с нулевым изменением количества")
+        # ch_qant = Change_qantity(**data)
+        # ch_qant.time_oper = datetime.now()
+        # ch_qant.save()
+        # print(model_to_dict(ch_qant))
--- Positions/views.py
+++ Positions/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
-from .func_for_change import change_info_pos
+from .func_for_change import change_info_pos, change_info_ch_type, change_test_new
 from .func_for_views import excel_to_dict, vvod_info_pos, vvod_info_group, nomenklatura_test, \
     xyz_test, vvod_info_xyz, obj_test, vvod_info_obj, \
     level_test, vvod_info_level, vvod_info_person, person_test, \
@@ -14,7 +14,7 @@ import sqlite3
 
 def index(request):
 
-#    change_info_pos()
+    change_info_pos()
 # positions = Positions._meta.get_fields()
 #     for i in excel_to_dict():
 #          vvod_info_pos(i)
@@ -28,9 +28,12 @@ def index(request):
 #         vvod_info_person(i)
 #     for i in obj_test:
 #         vvod_info_obj(i)
-#     for i in change_test:
-#         vvod_info_ch_type(i)
-    vvod_info_ch_qant(qant_test)
+#     for i in change_test_new:
+#         change_info_ch_type(i)
+#  #    for i in change_test:
+ #        vvod_info_ch_type(i)
+ #    vvod_info_ch_qant()
+
     return HttpResponse('OK')
 
 
