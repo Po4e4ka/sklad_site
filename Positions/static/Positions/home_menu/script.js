d6ee01c7d4da123cf8ece07d9734585373f4a34e
Po4e4ka <toolset@live.ru>
2021-02-22 12:51:05 +0300

    добавлена таблица как шаблон добавлена таблица работников добавлена таблица чейнджей
    Стабильная 0.06

--- Positions/static/Positions/home_menu/script.js
+++ Positions/static/Positions/home_menu/script.js
@@ -36,31 +36,32 @@ function init() {
   }
 }
 
-// function bubble() {
-//   ctx.clearRect(0,0,w,h);
-//
-//   for(var i = 0; i < arc; i++) {
-//     var li = lights[i];
-//
-//     ctx.beginPath();
-//     ctx.arc(li.x,li.y,li.size,0,Math.PI*2,false);
-//     ctx.fillStyle = li.c;
-//     ctx.fill();
-//
-//     li.x = li.x + li.toX * (time * 0.05);
-//     li.y = li.y + li.toY * (time * 0.05);
-//
-//     if(li.x > w) { li.x = 0; }
-//     if(li.y > h) { li.y = 0; }
-//     if(li.x < 0) { li.x = w; }
-//     if(li.y < 0) { li.y = h; }
-//   }
-//   if(time < speed) {
-//     time++;
-//   }
-//   timerID = setTimeout(bubble,1000/rate);
-// }
-init();;
+function bubble() {
+  ctx.clearRect(0,0,w,h);
+
+  for(var i = 0; i < arc; i++) {
+    var li = lights[i];
+
+    ctx.beginPath();
+    ctx.arc(li.x,li.y,li.size,0,Math.PI*2,false);
+    ctx.fillStyle = li.c;
+    ctx.fill();
+
+    li.x = li.x + li.toX * (time * 0.05);
+    li.y = li.y + li.toY * (time * 0.05);
+
+    if(li.x > w) { li.x = 0; }
+    if(li.y > h) { li.y = 0; }
+    if(li.x < 0) { li.x = w; }
+    if(li.y < 0) { li.y = h; }
+  }
+  if(time < speed) {
+    time++;
+  }
+  timerID = setTimeout(bubble,1000/rate);
+}
+init();
+// bubble();
 
 //navigation (this is my code)
 var animation = 'easeOutCubic';
--- Positions/urls.py
+++ Positions/urls.py
@@ -1,27 +1,14 @@
-"""sklad_site URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.index, name="index"),
-#    path('index/', views.index_group, name="index_group"),
-    # path('pos/', views.PosView, name="position"),
-    path('new/', views.NewView.as_view(), name="new"),
-    path("list/", views.PositionLisView.as_view(), name="list"),
-    path("omagad/", views.bd_func_vvod_start),
-    path("list/position<int:pos_id>/", views.PositionView.as_view(), name="position")
+    path('', views.index, name="index"),  # стартовая страница
+    path('new/', views.NewView.as_view(), name="new"),  # новая позиция
+    path("list/", views.PositionLisView.as_view(), name="list"),  # таблица позиций
+    path("persons/", views.PersonListView.as_view(), name="persons"),  # таблица персон
+    path("changes/", views.ChangeListView.as_view(), name="changes"), #таблица изменений
+    path("omagad/", views.bd_func_vvod_start),  # для тестов пост страница
+    path("list/position<int:pos_id>/", views.PositionView.as_view(), name="position")  # отображение страницы позиции
+
 ]
